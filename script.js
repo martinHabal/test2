387f59d274518572af777402f6807f76efb0846b
martinHabal <martin.habal@gmail.cz>
2023-11-13 11:06:59 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// let age = prompt("zadej věk")
+
+// if (age >= 18) {
+//     alert("jsi dospěký")
+// } else {
+//     alert("nejsi dospělý")
+// }
+
+/*
+
+
+*/
+
+let friends = ["Jarda", "Kamila", "Pepsa"]//indexuje se od nuly
+
+
+for (let i = 0; i < friends.length; i++) {
+
+    if (friends[i] === "Pepa") {
+        console.log(friends[i])
+    } else {
+        console.log(`Sorry pepa tam neni`)
+    }
+}
